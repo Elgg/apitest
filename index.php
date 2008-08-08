eae493aa1da999dee19f694058ac876e2c6e8926
Ben Werdmuller <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-08-08 16:31:50 +0000

    Sanitising author line for accuracy

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	 * 
 	 * @package ElggDevTools
 	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-	 * @author Marcus Povey <marcus@marcus-povey.co.uk>
+	 * @author Curverider Ltd <info@marcus-povey.co.uk>
 	 * @copyright Curverider Ltd 2008
 	 * @link http://elgg.com/
 	 */
